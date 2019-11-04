8774cf372d810cfaa05002e51079fef9c4412589
mathesjame <mathesjame@ecs.vuw.ac.nz>
2019-11-04 14:45:13 +1300

    Added search functionality

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+
 public class Main {
+    private static String DIRCIONARY_PATH = "data/words.txt";
 
-
-    WordStore store = new WordStore("");
+    private static WordStore store;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String wordToFind = "Test";
+
+        System.out.println("Loading Dictionary...");
+        store = new WordStore(DIRCIONARY_PATH);
+        System.out.println("...complete");
+
+        System.out.println("Searching dictionary for words containing \"" + wordToFind + "\"");
+        ArrayList<String> output = store.findContaining(wordToFind);
+
+        System.out.println("Found " + output.size() + " words:");
+        for(String s : output){
+            System.out.println(s);
+        }
     }
 
 
--- src/WordStore.java
+++ src/WordStore.java
@@ -1,7 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordStore {
 
+    ArrayList<String> allWords;
 
     WordStore(String wordFilePath){
+        allWords = new ArrayList<>();
 
+        try {
+            Scanner s = new Scanner(new File(wordFilePath));
+            while(s.hasNextLine()){
+                allWords.add(s.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: \"" + wordFilePath + "\"");
+        }
+    }
+
+    public ArrayList<String> findContaining(String find){
+        ArrayList<String> list = new ArrayList<>();
+
+        for(String s : allWords){
+            if(containedIn(find,s)){
+                list.add(s);
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * Is String a contained in String b
+     * @param a
+     * @param b
+     * @return
+     */
+    private boolean containedIn(String a, String b){
+        if(b.length() < a.length())return false;
+        int aInd = 0;
+        int bInd = 0;
+
+        while(bInd <= b.length()){
+            if(b.charAt(bInd) == a.charAt(aInd)){
+                aInd++;
+            }
+            if(aInd == a.length())return true;
+            bInd++;
+        }
+        return false;
     }
 }
